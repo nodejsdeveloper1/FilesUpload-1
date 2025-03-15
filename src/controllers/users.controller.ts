47b96adb63cef0166c075ff89563e5dc9db3d8f3
Vinod Kumar <mvinodkumar@mrsoftsolutions.com>
2025-03-15 11:23:34 +0530

    users Data

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -15,4 +15,14 @@ export class UsersController {
             res.status(400).json({error: error.message});
         }
     }
+
+    FetchUsers = async(req,res)=>{          
+        try{
+            const users = await this.userModel.find();
+            res.status(200).json(users);
+        }catch(error){
+            res.status(400).json({error: error.message});
+        }     
+    }          
+
 }
